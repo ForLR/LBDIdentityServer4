1104857287900204331d19a2c0a3ec9c141d5eef
烂布袋 <873448329@qq.com>
2019-09-16 20:58:33 +0800

    添加项目文件。

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LBDIdentityServer4
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public IConfiguration _configuration;
+
+        public Startup(IHostingEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            //services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients());
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to support static files
+            //app.UseStaticFiles();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add an MVC-based UI
+            //app.UseMvcWithDefaultRoute();
+        }
+    }
+}
